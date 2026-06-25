0931203772521a252e1f69b8e18e8c19bcbef735
n-r-cross <nicholaseagleproject@gmail.com>
2026-06-24 19:38:39 -0600

    Added equals and hashCode to ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -31,5 +33,19 @@ public class ChessPosition {
      */
     public int getColumn() {
         return p_col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return p_row == that.p_row && p_col == that.p_col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(p_row, p_col);
     }
 }
