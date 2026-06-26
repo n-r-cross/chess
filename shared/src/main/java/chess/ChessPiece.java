df6f447ec7a0be8e8a2971ecd7cbb330a3175de6
n-r-cross <nicholaseagleproject@gmail.com>
2026-06-25 20:11:23 -0600

    Added equals and hashCode for ChessPiece. Started designing pieceMoves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -46,6 +48,20 @@ public class ChessPiece {
         return p_type;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return p_color == that.p_color && p_type == that.p_type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(p_color, p_type);
+    }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -54,6 +70,52 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        //
+        Collection<ChessMove> c;
+        c = new ArrayList<ChessMove>();
+        switch(p_type){
+            case KING:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                for(int i = -1; i <= 1; i++) {
+                    for(int j = -1; j <= 1; j++) {
+                        if (ChessPosition.inBounds(myPosition.getRow(true) + i, myPosition.getColumn(true) + j)) {
+                            if (true) { // Add test if piece is at position
+                                ChessPosition e = new ChessPosition(myPosition.getRow(true) + i, myPosition.getColumn(true) + j);
+                                c.add(new ChessMove(myPosition, e, null));
+                            }
+                        }
+                    }
+                }
+                break;
+            case QUEEN:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                break;
+            case BISHOP:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                break;
+            case KNIGHT:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                break;
+            case ROOK:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                break;
+            case PAWN:
+                // Generate a possible position
+                // Check if position is inBounds
+                // Check if piece at position
+                break;
+        }
         throw new RuntimeException("Not implemented");
+
     }
 }
