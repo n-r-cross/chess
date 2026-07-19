377fbe7e0f149db1d3faf4501080cf5aa09d4862
n-r-cross <nicholaseagleproject@gmail.com>
2026-07-18 20:36:35 -0600

    Created unit tests for SQLUserDAO

---
+++ server/src/test/java/dataaccess/SQLUserDAOTest.java
@@ -0,0 +1,78 @@
+package dataaccess;
+
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SQLUserDAOTest {
+    @BeforeEach
+    void beforeEach() {
+        UserDAO userDAO = new SQLUserDAO();
+        userDAO.clear();
+    }
+
+    @Test
+    @DisplayName("Valid User Insertion")
+    void insertUserSuccess() {
+        UserDAO userDAO = new SQLUserDAO();
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.insertUser(new UserData("ga", "ga", "ga"));
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid User Insertion (username already exists)")
+    void insertUserFail() {
+        UserDAO userDAO = new SQLUserDAO();
+        UserData ud = new UserData("ga", "ga", "ga");
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.insertUser(ud);
+        });
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDAO.insertUser(ud);
+        });
+    }
+
+    @Test
+    @DisplayName("Valid Get User (user already exists)")
+    void getUserSuccess() {
+        UserDAO userDAO = new SQLUserDAO();
+        UserData ud = new UserData("ga", "ga", "ga");
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.insertUser(new UserData("ga", "ga", "ga"));
+        });
+        UserData response = null;
+        try {
+            response = userDAO.getUser("ga");
+        } catch (Exception e) {
+            fail();
+        }
+        Assertions.assertEquals(ud, response);
+    }
+
+    @Test
+    @DisplayName("Invalid Get User (user doesn't exist)")
+    void getUserFail() {
+        UserDAO userDAO = new SQLUserDAO();
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDAO.getUser("ga");
+        });
+    }
+
+    @Test
+    void clear() {
+        UserDAO userDAO = new SQLUserDAO();
+        UserData ud = new UserData("ga", "ga", "ga");
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.insertUser(new UserData("ga", "ga", "ga"));
+        });
+        userDAO.clear();
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDAO.getUser("ga");
+        });
+    }
+}
