8250df3bab581c62a13d17106989e09b627712c6
n-r-cross <nicholaseagleproject@gmail.com>
2026-07-21 20:16:37 -0600

    Modified code to use SQL DAOs

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -3,7 +3,7 @@ package service;
 import dataaccess.*;
 
 public class Service {
-    protected static UserDAO userData = new MemoryUserDAO();
-    protected static AuthDAO authData = new MemoryAuthDAO();
-    protected static GameDAO gameData = new MemoryGameDAO();
+    protected static UserDAO userData = new SQLUserDAO();
+    protected static AuthDAO authData = new SQLAuthDAO();
+    protected static GameDAO gameData = new SQLGameDAO();
 }
