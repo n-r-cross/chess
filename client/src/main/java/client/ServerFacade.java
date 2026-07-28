66f039a9c71a1e83a57cd58aafe3fc5960f6f86d
n-r-cross <nicholaseagleproject@gmail.com>
2026-07-28 10:47:05 -0600

    Wrote listGames tests and implemented listGames

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -5,6 +5,7 @@ import server.request.CreateRequest;
 import server.request.LoginRequest;
 import server.request.RegisterRequest;
 import server.result.CreateResult;
+import server.result.ListResult;
 import server.result.LoginResult;
 import server.result.RegisterResult;
 
@@ -85,7 +86,7 @@ public class ServerFacade {
         }
     }
 
-    public int createGame(String name, String authToken) throws Exception {
+    public CreateResult createGame(String name, String authToken) throws Exception {
         CreateRequest body = new CreateRequest(name);
         String bodyString = gson.toJson(body);
         String urlString = String.format(Locale.getDefault(), "http://%s:%d%s", serverUrl, port, "/game");
@@ -99,11 +100,29 @@ public class ServerFacade {
 
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         if (httpResponse.statusCode() >= 200 && httpResponse.statusCode() < 300) {
-            CreateResult result = gson.fromJson(httpResponse.body(), CreateResult.class);
-            return result.gameID();
+            return gson.fromJson(httpResponse.body(), CreateResult.class);
         } else {
             System.out.println(gson.fromJson(httpResponse.body(), ErrorResult.class).message());
             throw new Exception("Create game failed");
+        }
+    }
+
+    public ListResult listGames(String authToken) throws Exception {
+        String urlString = String.format(Locale.getDefault(), "http://%s:%d%s", serverUrl, port, "/game");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(urlString))
+                .timeout(java.time.Duration.ofMillis(5000))
+                .GET()
+                .header("Authorization", authToken)
+                .build();
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (httpResponse.statusCode() >= 200 && httpResponse.statusCode() < 300) {
+            return gson.fromJson(httpResponse.body(), ListResult.class);
+        } else {
+            System.out.println(gson.fromJson(httpResponse.body(), ErrorResult.class).message());
+            throw new Exception("List games failed");
         }
     }
 
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,9 +1,11 @@
 package client;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import server.Server;
-import server.request.LogoutRequest;
+import server.result.ListResult;
 import server.result.LoginResult;
 import service.ClearService;
 
@@ -41,7 +43,7 @@ public class ServerFacadeTests {
     @Test
     public void registerFail() {
         try {
-            Assertions.assertEquals(null, serverFacade.register(null, "ga", "ga"));
+            Assertions.assertNull(serverFacade.register(null, "ga", "ga"));
         } catch (Exception e) {
             fail();
         }
@@ -74,7 +76,7 @@ public class ServerFacadeTests {
     @Test
     public void loginFail() {
         try {
-            LoginResult lr = serverFacade.login("ga", "ga");
+            serverFacade.login("ga", "ga");
         } catch (Exception e) {
             fail();
         }
@@ -119,7 +121,7 @@ public class ServerFacadeTests {
 
         int id = -1;
         try {
-            id = serverFacade.createGame("light_cycles", token);
+            id = serverFacade.createGame("light_cycles", token).gameID();
         } catch (Exception e) {
             System.out.println(e.getMessage());
             fail();
@@ -127,5 +129,36 @@ public class ServerFacadeTests {
         Assertions.assertNotEquals(-1, id);
     }
 
+    @Test
+    public void listGamesSuccess() {
+        String token = "";
+        try {
+            token = serverFacade.register("ga", "ga", "ga").authToken();
+        } catch (Exception e) {
+            fail();
+        }
+        try {
+            Assertions.assertEquals(ListResult.class, serverFacade.listGames(token).getClass());
+            serverFacade.createGame("light_cycles", token);
+            Assertions.assertEquals(ListResult.class, serverFacade.listGames(token).getClass());
+            GameData data = serverFacade.listGames(token).games().getFirst();
+            Assertions.assertEquals(new GameData(1, null, null, "light_cycles", new ChessGame()), data);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void listGamesFail() {
+        Assertions.assertThrows(Exception.class, () -> serverFacade.listGames("fake-auth-token"));
+    }
+
+    @Test
+    public void joinGameSuccess() {
+    }
+
+    @Test
+    public void joinGameFail() {
+    }
 
 }
